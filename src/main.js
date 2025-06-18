c22762c5f09a202f2a3812939aa68024eb2ca71f
oo77 <54396118+oo77@users.noreply.github.com>
2025-06-18 15:14:43 +0500

    c

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router'
-import i18n from './i18n'
+import router from './router/index.js'
+import i18n from './i18n/index.js'
 import AOS from 'aos'
 
 const app = createApp(App)
